317b237326e0dba6e4a666a0ead8557180017d0e
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-11-13 21:28:58 +0100

    feat: cards function

--- js/script.js
+++ js/script.js
@@ -36,10 +36,46 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
-
+// console.log(teamMembers);
 
 // Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata  in cui mostrare una card per ciascun componente.
 // (trovate l’array del team all’interno della cartella in allegato)
 // **Bonus**
 // - Rendere l’esercizio responsive, mandando a capo le card
-// - Aggiungere un form di aggiunta membri che permetta di visualizzare il nuovo membro sulla pagina (usate una foto qualunque, anche vostra se volete sentirvi parte del team! :sorridere:)
+// - Aggiungere un form di aggiunta membri che permetta di visualizzare il nuovo membro sulla pagina (usate una foto qualunque, anche vostra se volete sentirvi parte del team
+
+
+//Prelevo gli elementi dall'Html
+ const teamCointainerElem = document.querySelector(".team-container")
+
+
+//Creo la card
+const createCard = ({name, role, email, img}) => {
+  return `
+  <div class="card col-md-4 p-3 m-2" style="width: 18rem;">
+    <img src="${img}" class="card-img-top" alt="${name}">
+        <div class="card-body">
+           <h5 class="card-title">${name}</h5>
+           <p class="card-text">${role}</p>
+        </div>
+        <div class="card-body">
+           <a href="#" class="card-link">${email}</a>
+       </div>
+    </div>
+ `;
+};
+// console.log(createCard)
+
+
+//Stampo le card dinamiche
+const renderTeam = () => {
+  let items = "";
+  for (let i = 0; i < teamMembers.length; i++) {
+    const card = createCard(teamMembers[i]);
+    items += card;
+  }
+teamCointainerElem.innerHTML = items;
+};
+
+//Invoco la funzione
+renderTeam();
